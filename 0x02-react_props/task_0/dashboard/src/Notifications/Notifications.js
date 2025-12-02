5f8080d560520398991b42194e7ae51aeb2bd358
joekariuki3 <joekariuki3@gmail.com>
2025-12-02 23:08:51 +0300

    feat: enhance Notifications component with displayDrawer prop and update styles

--- 0x02-react_props/task_0/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_0/dashboard/src/Notifications/Notifications.js
@@ -2,39 +2,53 @@ import React from "react";
 import "./Notifications.css";
 import { getLatestNotification } from "../utils/utils";
 import NotificationItem from "../NotificationItem/NotificationItem";
+import PropTypes from "prop-types";
 // import closeIcon from "./close-icon.png";
 
-const Notification = () => {
+const Notification = ({ displayDrawer = false }) => {
   function buttonClicked() {
     console.log("Close button has been clicked");
   }
   return (
-    <div className="Notifications">
-      <p>Here is the list of notifications</p>
-      <ul>
-        <NotificationItem
-          type="default"
-          html={{ __html: "New course available" }}
-          value=""
-        />
-        <NotificationItem
-          type="urgent"
-          html={{ __html: "New resume available" }}
-          value=""
-        />
-        <NotificationItem
-          type="urgent"
-          html={{ __html: getLatestNotification() }}
-          value=""
-        />
-      </ul>
-      <button
-        style={{ align: "right", ariaLabel: "Close" }}
-        onClick={buttonClicked}
-      >
-        {/* <img src={closeIcon} alt="close icon"/> */}X
-      </button>
-    </div>
+    <>
+      <div className="menuItem">
+        <p>Your notifications</p>
+      </div>
+      {displayDrawer ? (
+        <div className="Notifications">
+          <p>Here is the list of notifications</p>
+          <ul>
+            <NotificationItem
+              type="default"
+              html={{ __html: "New course available" }}
+              value=""
+            />
+            <NotificationItem
+              type="urgent"
+              html={{ __html: "New resume available" }}
+              value=""
+            />
+            <NotificationItem
+              type="urgent"
+              html={{ __html: getLatestNotification() }}
+              value=""
+            />
+          </ul>
+          <button
+            style={{ align: "right", ariaLabel: "Close" }}
+            onClick={buttonClicked}
+            className="Notifications-close-button"
+          >
+            {/* <img src={closeIcon} alt="close icon"/> */}X
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
+    </>
   );
+};
+Notification.prototype = {
+  displayDrawer: PropTypes.bool,
 };
 export default Notification;
--- 0x02-react_props/task_0/dashboard/src/Notifications/Notifications.test.js
+++ 0x02-react_props/task_0/dashboard/src/Notifications/Notifications.test.js
@@ -8,13 +8,13 @@ describe("Notifications", () => {
   });
 
   it("Notifications renders with three list items", () => {
-    render(<Notifications />);
+    render(<Notifications displayDrawer={true} />);
     const listElement = screen.getByRole("list");
     expect(listElement.children.length).toBe(3);
   });
 
   it("Notification renders with a text 'Here is the list of notifications'", () => {
-    render(<Notifications />);
+    render(<Notifications displayDrawer={true} />);
     const paragraphElement = screen.getByText(
       "Here is the list of notifications"
     );
@@ -22,14 +22,41 @@ describe("Notifications", () => {
   });
 
   it("renders NotificationItem component", () => {
-    render(<Notifications />);
+    render(<Notifications displayDrawer={true} />);
     const listElement = screen.getByRole("list");
     expect(listElement.children.length).toBe(3);
   });
 
   it("renders NotificationItem component and first child shows correct text content", () => {
-    render(<Notifications />);
+    render(<Notifications displayDrawer={true} />);
     const firstListItemElement = screen.getByText("New course available");
     expect(firstListItemElement).toBeInTheDocument();
+  });
+
+  it("renders a div with class menuItem when property displayDrawer is false", () => {
+    render(<Notifications />); // displayDrawer prop is false by default
+    const menuElement = screen.getByText("Your notifications");
+    expect(menuElement).toBeInTheDocument();
+    expect(menuElement.parentElement).toHaveClass("menuItem");
+  });
+
+  it("does not renders a div with class Notifications when property displayDrawer is false", () => {
+    render(<Notifications />); // displayDrawer prop is false by default
+    const menuElement = screen.queryByText("Here is the list of notifications");
+    expect(menuElement).toBe(null);
+  });
+
+  it("renders a div with class menuItem when property displayDrawer is true", () => {
+    render(<Notifications displayDrawer={true} />);
+    const menuElement = screen.getByText("Your notifications");
+    expect(menuElement).toBeInTheDocument();
+    expect(menuElement.parentElement).toHaveClass("menuItem");
+  });
+
+  it("renders a div with class Notifications when property displayDrawer is true", () => {
+    render(<Notifications displayDrawer={true} />);
+    const menuElement = screen.queryByText("Here is the list of notifications");
+    expect(menuElement).toBeInTheDocument();
+    expect(menuElement.parentElement).toHaveClass("Notifications");
   });
 });
