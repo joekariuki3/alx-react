c882fc54dd7e6aed91babb59a706b99278fc61d5
joekariuki3 <joekariuki3@gmail.com>
2025-12-02 23:11:51 +0300

    feat: add CourseList component with course details table

---
+++ 0x02-react_props/task_0/dashboard/src/CourseList/CourseList.js
@@ -0,0 +1,22 @@
+import CourseListRow from "./CourseListRow";
+import "./CourseList.css";
+const CourseList = () => {
+  return (
+    <table id="CourseList">
+      <thead>
+        <CourseListRow isHeader={true} textFirstCell="Available courses" />
+        <CourseListRow
+          isHeader={true}
+          textFirstCell="Course name"
+          textSecondCell="Credit"
+        />
+      </thead>
+      <tbody>
+        <CourseListRow textFirstCell="ES6" textSecondCell="60" />
+        <CourseListRow textFirstCell="Webpack" textSecondCell="20" />
+        <CourseListRow textFirstCell="React" textSecondCell="40" />
+      </tbody>
+    </table>
+  );
+};
+export default CourseList;
