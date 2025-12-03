b61fa5c071f5275122e1da2bce8c949aa9abbf86
joekariuki3 <joekariuki3@gmail.com>
2025-12-03 13:40:46 +0300

    feat: update App component to pass listNotifications and listCourses props to Notifications and CourseList

--- 0x02-react_props/task_0/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -4,15 +4,44 @@ import Login from "../Login/Login";
 import Footer from "../Footer/Footer";
 import CourseList from "../CourseList/CourseList";
 import PropTypes from "prop-types";
+import { getLatestNotification } from "../utils/utils";
 
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+const listNotifications = [
+  {
+    id: 1,
+    type: "default",
+    html: { __html: "New course available" },
+    value: "",
+  },
+  {
+    id: 2,
+    type: "urgent",
+    html: { __html: "New resume available" },
+    value: "",
+  },
+  {
+    id: 3,
+    type: "urgent",
+    html: { __html: getLatestNotification() },
+    value: "",
+  },
+];
 const App = ({ isLoggedIn = false }) => {
   return (
     <>
-      <Notifications displayDrawer={false} />
+      <Notifications
+        displayDrawer={false}
+        listNotifications={listNotifications}
+      />
       <div className="App">
         <Header />
         <main className="App-body">
-          {isLoggedIn ? <CourseList /> : <Login />}
+          {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
         </main>
         <Footer />
       </div>
