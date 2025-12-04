34d257931cc21b97742eef933823ec9460a6aae0
joelmuhoho <joekariuki3@gmail.com>
2025-12-04 12:25:35 +0300

    feat: add keyboard shortcut for logout and improve tests with alert mock

--- 0x03-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
 describe("App", () => {
@@ -39,6 +40,17 @@ describe("App", () => {
 });
 
 describe("App when isLoggedIn is true", () => {
+  // Mock alert
+  let alertSpy;
+
+  beforeEach(() => {
+    alertSpy = jest.spyOn(window, "alert").mockImplementation(() => {});
+  });
+
+  afterEach(() => {
+    alertSpy.mockRestore();
+  });
+
   it("renders CourseList component with a table element", () => {
     render(<App isLoggedIn={true} />);
     const tableElement = screen.getByRole("table");
@@ -51,5 +63,13 @@ describe("App when isLoggedIn is true", () => {
       "Login to access the full dashboard"
     );
     expect(loginElementParagraph).toBe(null);
+  });
+
+  it("when keys `control` and `h` are pressed logOut function is called and alert function is called with string Logging you out", async () => {
+    render(<App />);
+
+    const user = userEvent.setup();
+    await user.keyboard("{Control>}h{/Control}");
+    expect(window.alert).toHaveBeenCalledWith("Logging you out");
   });
 });
