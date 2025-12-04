56df59d1a400601f119824c44e38ba5938b6fe9c
joelmuhoho <joekariuki3@gmail.com>
2025-12-04 16:07:32 +0300

    refactor: convert NotificationItem to class-based component and enhance prop handling

--- 0x03-React_component/task_2/dashboard/src/NotificationItem/NotificationItem.js
+++ 0x03-React_component/task_2/dashboard/src/NotificationItem/NotificationItem.js
@@ -1,19 +1,27 @@
 import "./NotificationItem.css";
 import PropTypes from "prop-types";
-
-const NotificationItem = ({ type = "default", html, value }) => {
-  return (
-    <li
-      data-notification-type={type}
-      value={value}
-      dangerouslySetInnerHTML={html}
-    ></li>
-  );
+import { Component } from "react";
+class NotificationItem extends Component {
+  render() {
+    return (
+      <li
+        data-notification-type={this.props.type}
+        value={this.props.value}
+        dangerouslySetInnerHTML={this.props.html}
+        onClick={() => this.props.markAsRead(this.props.id)}
+      ></li>
+    );
+  }
+}
+NotificationItem.defaultProps = {
+  type: "default",
+  markAsRead: (id) => {},
 };
-
 NotificationItem.propTypes = {
   type: PropTypes.string.isRequired,
   html: PropTypes.shape({ __html: PropTypes.string }),
   value: PropTypes.string,
+  id: PropTypes.number,
+  markAsRead: PropTypes.func,
 };
 export default NotificationItem;
